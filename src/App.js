01a10a6b795b1ab6992986d9bd73467195764c1d
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-05 10:56:14 -0500

    coparison about hooks functional component and class component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,30 @@
 // always at the highest level of the component
 // they are only called in 2 parts:
 // React component and Custo hooks
-// when we create a custom hook it always has to start with 'use and than the other part.
+// when we create a custom hook it always has to start with 'use and than   the other part.
 
+// comparison about class component and hooks functional component
+// Class component
+import { Component } from 'react'
+class App extends Component {
+  state = { contador : 0 }
+  incrementar = () => {
+    this.setState({ contador: this.state.contador + 1 })
+  }
+  render() {
+    return(
+      <div>
+        contador: {this.state.contador}
+        <button onClick={this.incrementar}>Incrementar</button>
+      </div>
+    )
+  }
+}
+
+
+// Hook functional component
+
+/* 
 import { useState } from "react"
 
 const App = () => {
@@ -18,5 +40,6 @@ const App = () => {
     </div>
   )
 }
+*/
 
-export default App
+export default App 
